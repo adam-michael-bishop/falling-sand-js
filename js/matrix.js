00bdb8a396fe330f4925ce15f4928f142d9cb397
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-18 16:45:03 -0600

    Features -Added a function to create a 2d array to store the coordinates of the next cell an element should travel to -Added a function to iterate through the next step 2d array and swap the cells of elements that will move -Added a function to handle the element swapping -Edited the Matrix class to fill all cells with Void element by default

--- js/matrix.js
+++ js/matrix.js
@@ -1,5 +1,5 @@
 "use strict"
-
+import * as Element from "./elements.js";
 export {Matrix};
 
 class Matrix {
@@ -13,6 +13,7 @@ class Matrix {
             }
             return matrix
         })();
+        this.fill(new Element.Void());
     }
     fill(element) {
         for (const cellRow of this.array2d) {
@@ -20,5 +21,35 @@ class Matrix {
                 cellRow[i] = element;
             }
         }
+    }
+    generateNextStepMatrix() {
+        let arr2d = [];
+        for (let i = 0; i < this.array2d.length; i++) {
+            arr2d.push([]);
+            for (let j = 0; j < this.array2d[i].length; j++) {
+                if (this.array2d[i][j].step !== undefined) {
+                    let nextStepCoords = this.array2d[i][j].step(j, i, this.array2d);
+                    arr2d[i].push(nextStepCoords);
+                } else {
+                    arr2d[i].push('');
+                }
+            }
+        }
+        return arr2d;
+    }
+    updateSteps(stepsArr2d) {
+        for (let i = 0; i < this.array2d.length; i++) {
+            for (let j = 0; j < this.array2d[i].length; j++) {
+                if (stepsArr2d[i][j] !== '') {
+                    let swappedElement = this.swapElementPositions(this.array2d[i][j], stepsArr2d[i][j]);
+                    this.array2d[i][j] = swappedElement;
+                }
+            }
+        }
+    }
+    swapElementPositions(element, coords) {
+        let swappedElement = this.array2d[coords.y][coords.x];
+        this.array2d[coords.y][coords.x] = element;
+        return swappedElement;
     }
 }
