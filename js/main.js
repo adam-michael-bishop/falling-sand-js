fdb316eca5eb39500905154892daf572dec14a5f
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-17 23:01:11 -0600

    -Added a function to draw the matrix contents into the canvas -Draw function updated on an interval -Added a function to be called on each tick interval (each frame)

--- js/main.js
+++ js/main.js
@@ -3,17 +3,35 @@
 import * as Matrix from "./matrix.js";
 import * as Elements from "./elements.js";
 
-const pixelsPerUnit = 4;
-const canvasWidth = 700;
+const pixelToMatrixRatio = 2;
+const canvasWidth = 750;
 const canvasHeight = 500;
 const canvasId = 'game-window';
 const canvas = document.createElement('canvas');
+const context = canvas.getContext("2d");
+const frameRate = 60;
+const updateRateMS = 1000 / frameRate;
+const matrix = new Matrix.Matrix(canvasWidth / pixelToMatrixRatio, canvasHeight / pixelToMatrixRatio);
 
-$('body').prepend(canvas);
+function tick() {
+    drawMatrixToContext();
+}
+
+//Need to link element color type with the context.fillStyle
+function drawMatrixToContext() {
+    for (let i = 0; i < matrix.height; i++) {
+        for (let j = 0; j < matrix.width; j++) {
+            context.fillStyle = "initial";
+            context.fillRect(j * pixelToMatrixRatio, i * pixelToMatrixRatio, pixelToMatrixRatio, pixelToMatrixRatio);
+        }
+    }
+}
+
+$('body').prepend(canvas).css("background-color", "grey");
 $('canvas').attr({
     width: `${canvasWidth}`,
     height: `${canvasHeight}`,
     id: canvasId,
 });
 
-console.log($('body'));
+setInterval(tick, updateRateMS);
