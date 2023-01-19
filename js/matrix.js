46c5c8fcfa2ac030dfe33e365be041ab913292e4
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-19 14:06:07 -0600

    Refactor -Removed fill method in Matrix -Filled matrix array with void during construction of array2d

--- js/matrix.js
+++ js/matrix.js
@@ -9,26 +9,26 @@ class Matrix {
         this.array2d = (() => {
             const matrix = [];
             for (let i = 0; i < this.height; i++) {
-                matrix[i] = new Array(this.width);
+                matrix.push([]);
+                for (let j = 0; j < this.width; j++) {
+                    matrix[i].push(new Element.Void(j, i));
+                }
             }
             return matrix
         })();
-        this.fill(new Element.Void());
     }
-    fill(element) {
-        for (const cellRow of this.array2d) {
-            for (let i = 0; i < cellRow.length; i++) {
-                cellRow[i] = element;
-            }
-        }
-    }
-    generateNextStepMatrix() {
+    /*TODO:
+    * Refactor the generateNextStepMatrix to just call the step function for each element
+    * Maybe create a shouldStep method to determine if an element has room to move
+    * This would make the step method on the element simpler, as it would only need to call it if the element should move
+     */
+    moveAllElements() {
         let arr2d = [];
         for (let i = 0; i < this.array2d.length; i++) {
             arr2d.push([]);
             for (let j = 0; j < this.array2d[i].length; j++) {
-                if (this.array2d[i][j].step !== undefined) {
-                    let nextStepCoords = this.array2d[i][j].step(j, i, this.array2d);
+                if (this.array2d[i][j].move !== undefined) {
+                    let nextStepCoords = this.array2d[i][j].move(j, i, this.array2d);
                     arr2d[i].push(nextStepCoords);
                 } else {
                     arr2d[i].push('');
@@ -37,7 +37,7 @@ class Matrix {
         }
         return arr2d;
     }
-    updateSteps(stepsArr2d) {
+    updateElementPositions(stepsArr2d) {
         for (let i = 0; i < this.array2d.length; i++) {
             for (let j = 0; j < this.array2d[i].length; j++) {
                 if (stepsArr2d[i][j] !== '') {
