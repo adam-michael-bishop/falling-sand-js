e68c85901e35a1202b88dd8c8f3e6f4ea990586e
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-19 16:51:38 -0600

    Refactor -Moved logic to determine if an element should move to the shouldMove method -Working on refactoring the move method

--- js/elements.js
+++ js/elements.js
@@ -30,46 +30,62 @@ class Sand extends Element {
         this.name = "sand";
         this.color = sandColor;
         this.state = "solid";
+        this.isMoving = false;
     }
-    move(xIndex, yIndex, arr2d) {
-        const coordsCurrent = {y: yIndex, x: xIndex};
-        const coordsCellBelow = {y: yIndex + 1, x: xIndex};
-        const coordsCellBottomLeft = {y: yIndex + 1, x: xIndex - 1};
-        const coordsCellBottomRight = {y: yIndex + 1, x: xIndex + 1};
-        let cellBelow;
-        let cellBottomLeft;
-        let cellBottomRight;
+    move(matrix) {
+        // const coordsCurrent = {y: yIndex, x: xIndex};
+        // const coordsCellBelow = {y: yIndex + 1, x: xIndex};
+        // const coordsCellBottomLeft = {y: yIndex + 1, x: xIndex - 1};
+        // const coordsCellBottomRight = {y: yIndex + 1, x: xIndex + 1};
+        // let cellBelow;
+        // let cellBottomLeft;
+        // let cellBottomRight;
+        //
+        // if (yIndex !== arr2d.length - 1) {
+        //     cellBelow = arr2d[yIndex + 1][xIndex];
+        // }
+        // if (xIndex !== 0 && yIndex !== arr2d.length - 1) {
+        //     cellBottomLeft = arr2d[yIndex + 1][xIndex - 1];
+        // }
+        // if (xIndex !== arr2d[yIndex].length - 1 && yIndex !== arr2d.length - 1) {
+        //     cellBottomRight = arr2d[yIndex + 1][xIndex + 1];
+        // }
+        //
+        // if (cellBelow === undefined){
+        //     return coordsCurrent;
+        // } else if (cellBelow.state === 'solid') {
+        //     if (cellBottomRight === undefined) {
+        //         return coordsCellBottomLeft;
+        //     } else if (cellBottomLeft === undefined) {
+        //         return coordsCellBottomRight;
+        //     } else if (Math.random() < .5){
+        //         return coordsCellBottomLeft;
+        //     } else {
+        //         return coordsCellBottomRight;
+        //     }
+        // } else {
+        //     return coordsCellBelow;
+        // }
+        if (!this.shouldMove(matrix)) {
+            return //don't move
+        }
+        if (matrix.array2d[this.y + 1][this.x].state !== "solid") {
 
-        if (yIndex !== arr2d.length - 1) {
-            cellBelow = arr2d[yIndex + 1][xIndex];
-        }
-        if (xIndex !== 0 && yIndex !== arr2d.length - 1) {
-            cellBottomLeft = arr2d[yIndex + 1][xIndex - 1];
-        }
-        if (xIndex !== arr2d[yIndex].length - 1 && yIndex !== arr2d.length - 1) {
-            cellBottomRight = arr2d[yIndex + 1][xIndex + 1];
-        }
-
-        if (cellBelow === undefined){
-            return coordsCurrent;
-        } else if (cellBelow.state === 'solid') {
-            if (cellBottomRight === undefined) {
-                return coordsCellBottomLeft;
-            } else if (cellBottomLeft === undefined) {
-                return coordsCellBottomRight;
-            } else if (Math.random() < .5){
-                return coordsCellBottomLeft;
-            } else {
-                return coordsCellBottomRight;
-            }
-        } else {
-            return coordsCellBelow;
         }
     }
     shouldMove(matrix) {
-        if (this.y >= matrix.height) {
+        const cellBelow = matrix.array2d[this.y + 1][this.x];
+        const cellBottomRight = matrix.array2d[this.y + 1][this.x + 1];
+        const cellBottomLeft = matrix.array2d[this.y + 1][this.x - 1];
+
+        if ((this.y >= matrix.height) ||
+            (cellBelow.state === "solid" && cellBottomRight.state === "solid" && cellBottomLeft.state === "solid") ||
+            (this.x === matrix.width && cellBottomLeft.state === "solid" && cellBelow.state === "solid") ||
+            (this.x === 0 && cellBottomRight.state === "solid" && cellBelow.state === "solid")) {
+            this.isMoving = false;
             return false
         }
+        this.isMoving = true;
         return true
     }
 }
