b58c32690088a0065f92110f79b32334bfde5952
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-21 18:20:43 -0600

    Refactor and Features -moveAllElements renamed to setNewCoordsForAllElements -updateElementPositions uses getElementFromCoords method now -swapElementPositions refactored to account for velocity -getElementFromCoords added to simplify referring to the contents of a specific cell in 2d array

--- js/matrix.js
+++ js/matrix.js
@@ -22,34 +22,78 @@ class Matrix {
     * Maybe create a shouldStep method to determine if an element has room to move
     * This would make the step method on the element simpler, as it would only need to call it if the element should move
      */
-    moveAllElements() {
+    setNewCoordsForAllElements() {
         let arr2d = [];
         for (let i = 0; i < this.array2d.length; i++) {
             arr2d.push([]);
             for (let j = 0; j < this.array2d[i].length; j++) {
-                if (this.array2d[i][j].move !== undefined) {
-                    let nextStepCoords = this.array2d[i][j].move(j, i, this.array2d);
-                    arr2d[i].push(nextStepCoords);
+                let element = this.getElementFromCoords(j, i);
+                if (element instanceof Element.Void) {
+                    arr2d[i].push("");
                 } else {
-                    arr2d[i].push('');
+                    arr2d[i].push(element.getCoordsToNewMovePosition(this, element.shouldMove(this)));
                 }
             }
         }
-        return arr2d;
+        // console.log(arr2d);
+        return arr2d
     }
-    updateElementPositions(stepsArr2d) {
+    updateElementPositions(arr2d) {
         for (let i = 0; i < this.array2d.length; i++) {
             for (let j = 0; j < this.array2d[i].length; j++) {
-                if (stepsArr2d[i][j] !== '') {
-                    let swappedElement = this.swapElementPositions(this.array2d[i][j], stepsArr2d[i][j]);
-                    this.array2d[i][j] = swappedElement;
+                let element = this.getElementFromCoords(j, i);
+                let newCords = arr2d[i][j];
+                // console.log(newCords);
+                if (!(element instanceof Element.Void)) {
+                    if ((i !== newCords[1] || j !== newCords[0]) && newCords !== "") {
+                        let vector = element.shouldMove(this);
+                        this.swapElementPositions(element, newCords, vector);
+                    }
                 }
             }
         }
     }
-    swapElementPositions(element, coords) {
-        let swappedElement = this.array2d[coords.y][coords.x];
-        this.array2d[coords.y][coords.x] = element;
-        return swappedElement;
+    swapElementPositions(element, newCoords, vector) {
+        // console.log(element);
+        // console.log(newCoords);
+        // console.log(vector);
+        // while (element.x !== newCoords[0] || element.y !== newCoords[1]) {
+        //     let nextElement = this.getElementFromCoords(element.x, element.y, vector)
+        //     if (nextElement instanceof Element.Solid) {
+        //         console.log('break');
+        //         break
+        //     }
+        //     this.array2d[element.y][element.x] = nextElement;
+        //     console.log(element);
+        //     element.x += vector[0];
+        //     element.y += vector[1];
+        //     this.array2d[element.y][element.x] = element;
+        // }
+        for (let i = 0; i < element.velocity; i++) {
+            let nextElement = this.getElementFromCoords(element.x, element.y, vector);
+            if (nextElement instanceof Element.Solid || nextElement === undefined) {
+                return
+            }
+            nextElement.x = element.x;
+            nextElement.y = element.y;
+            this.setElementAtCoordsByVector(element, vector);
+            this.array2d[nextElement.y][nextElement.x] = nextElement;
+            i++;
+        }
+    }
+    getElementFromCoords(x, y, vector = [0, 0]) {
+        // console.log(JSON.stringify(this.array2d[y][x]));
+        let xCoords = x + vector[0];
+        let yCoords = y + vector[1];
+        if (xCoords >= this.width || xCoords < 0 || yCoords >= this.height || yCoords < 0) {
+            return undefined
+        }
+        // console.log(xCoords, yCoords);
+        return this.array2d[yCoords][xCoords];
+    }
+    setElementAtCoordsByVector(element, vector) {
+        element.x += vector[0];
+        element.y += vector[1];
+        this.array2d[element.y][element.x] = element;
     }
 }
