c3514353cdf189a592d228e70cd6f1033da0e1be
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-21 23:34:07 -0600

    Features -Added a paint to canvas feature that allows user to paint an element by holding mouse 1 on the canvas -Added a faucet to fill canvas with water

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ import * as Matrix from "./matrix.js";
 import * as Element from "./elements.js";
 
 const pixelToMatrixRatio = 4;
-const canvasWidth = 500;
-const canvasHeight = 400;
+const canvasWidth = 1000;
+const canvasHeight = 800;
 const canvasId = 'game-window';
 const canvas = document.createElement('canvas');
 const context = canvas.getContext("2d");
@@ -13,10 +13,16 @@ const frameRate = 60;
 const updateRateMS = 1000 / frameRate;
 const matrix = new Matrix.Matrix(canvasWidth / pixelToMatrixRatio, canvasHeight / pixelToMatrixRatio);
 let tickCount = 0;
+let togglePause = false;
+let toggleFaucet = false;
+let mouseHeld = undefined;
+let mousePosition;
 
 function tick() {
-    if (tickCount % 2 === 0 && tickCount < 500) {
-        matrix.array2d[0][5] = new Element.Sand(5, 0);
+    if (toggleFaucet) {
+        matrix.array2d[0][5] = new Element.Water(5, 0);
+        matrix.array2d[0][7] = new Element.Water(7, 0);
+        matrix.array2d[0][9] = new Element.Water(9, 0);
     }
     matrix.updateElementPositions(matrix.setNewCoordsForAllElements());
     context.clearRect(0, 0, canvasWidth, canvasHeight);
@@ -24,7 +30,6 @@ function tick() {
     tickCount++
 }
 
-//Need to link element color type with the context.fillStyle
 function drawMatrixToContext() {
     for (let i = 0; i < matrix.height; i++) {
         for (let j = 0; j < matrix.width; j++) {
@@ -33,30 +38,67 @@ function drawMatrixToContext() {
         }
     }
 }
+function getMousePos(canvas, evt) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
 
-$('body').prepend(canvas).css("background-color", "grey");
-$('canvas').css("border", "black solid 1px")
+function getCellFromMousePos(mousePos) {
+    let xPos = Math.floor((mousePos.x / pixelToMatrixRatio));
+    let yPos = Math.floor((mousePos.y / pixelToMatrixRatio));
+    // console.log(xPos, yPos, mousePos);
+    matrix.setElementAtCoordsByVector(new Element.Stone(xPos, yPos), [0, 0]);
+}
+
+$('body').prepend(canvas).css("background-color", "black");
+$('canvas').css("border", "white solid 1px")
     .attr({
         width: `${canvasWidth}`,
         height: `${canvasHeight}`,
         id: canvasId,
+    })
+    .after("<button id='pause-sim'>pause</button>" +
+        "<button id='toggle-faucet'>faucet</button>");
+$('#pause-sim').click(function (){
+    if (!togglePause) {
+        clearInterval(gameTick);
+        togglePause = true;
+    } else {
+        gameTick = setInterval(tick, updateRateMS);
+        togglePause = false;
+    }
+});
+
+$('#toggle-faucet').click(function () {
+    toggleFaucet = !toggleFaucet;
+})
+
+$('canvas').mousedown(function (){
+    mouseHeld = setInterval(function () {
+        getCellFromMousePos(mousePosition);
+    }, 10);
+})
+    .mouseup(function (){
+        clearInterval(mouseHeld);
+    })
+    .mousemove(function (event){
+    mousePosition = getMousePos(canvas, event);
+})
+    .mouseout(function (){
+        clearInterval(mouseHeld);
     });
+
+let gameTick = setInterval(tick, updateRateMS);
 
 
 // matrix.updateSteps(matrix.generateNextStepMatrix());
 // drawMatrixToContext();
 // matrix.array2d[0][10] = new Element.Sand(10, 0);
 
-// matrix.updateElementPositions(matrix.setNewCoordsForAllElements());
-// drawMatrixToContext();
-// matrix.updateElementPositions(matrix.setNewCoordsForAllElements());
-// drawMatrixToContext()
-// matrix.updateElementPositions(matrix.setNewCoordsForAllElements());
-// drawMatrixToContext()
-// matrix.updateElementPositions(matrix.setNewCoordsForAllElements());
-// drawMatrixToContext()
 // console.log(matrix.array2d[0][10].step(10,0,matrix.array2d));
-setInterval(tick, updateRateMS);
 // console.log(matrix);
 // let test = matrix.getElementFromCoords(10, 0);
 // console.log(test.getCoordsToNewMovePosition(matrix, test.shouldMove(matrix)));
